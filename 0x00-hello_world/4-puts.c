a34e3c38ec94a42e842828442be4a75b2f71bd6f
Martin Kariuki <kariuki.qwest@gmail.com>
2022-10-17 08:31:48 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Entry point
- *  
+ *
  * Return: Always 0 (Success)
  */
 
@@ -11,7 +11,7 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); 
+	return (0);
 
-	   
+
 }
