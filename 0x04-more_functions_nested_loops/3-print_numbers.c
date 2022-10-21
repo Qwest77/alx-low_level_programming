432d2fce5116ac1015f5fba4a260d83c05acace4
Martin Kariuki <kariuki.qwest@gmail.com>
2022-10-21 05:06:00 -0700

    Task 5

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,11 @@
 
 void print_numbers(void)
 {
-	int a = 0;
+	int n;
 
-	while (a < 10)
-		_putchar(a++ + '0');
-
+	for (n = 0; n <= 9; n++)
+	{
+	_putchar((n % 10) + '0');
+	}
 	_putchar('\n');
 }
