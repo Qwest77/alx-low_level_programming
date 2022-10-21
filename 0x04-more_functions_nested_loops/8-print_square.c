22204c820ffaa352a621d57dc7553c618843f1bb
Martin Kariuki <kariuki.qwest@gmail.com>
2022-10-21 07:12:15 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_square - prints a square
@@ -8,7 +9,7 @@ void print_square(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	else
 	{
@@ -19,9 +20,9 @@ void print_square(int size)
 
 			for (j = 0; j < size; j++)
 			{
-				_putchar('#');
+				putchar('#');
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
