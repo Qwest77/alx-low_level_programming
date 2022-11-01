141c5023d5642c3cb5baa59bb01e8f140639cdcb
Martin Kariuki <kariuki.qwest@gmail.com>
2022-11-01 08:09:22 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,13 +7,13 @@
  * @n: number of bytes to be changed
  * Return: changed array with new value for n bytes
  */
-har *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
 	for (; n > 0; i++)
 	{
-		s[i] = b
+		s[i] = b;
 		n--;
 	}
 	return (s);
