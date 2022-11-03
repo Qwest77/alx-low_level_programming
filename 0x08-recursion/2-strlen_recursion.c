26fbef6b191fc09a03a21d6316465fcf49bb4f7f
Martin Kariuki <kariuki.qwest@gmail.com>
2022-11-03 06:45:48 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+/*
+ * File: 2-strain_recursion.c
+ * Auth: smane
+ */
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be mearsured.
+ * Return: The length of the string.
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+}
