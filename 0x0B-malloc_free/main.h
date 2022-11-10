4248940ea65e4e7641c35cdf6318ed3e2df9e9d5
Martin Kariuki <kariuki.qwest@gmail.com>
2022-11-09 23:51:58 -0800

    task 0

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,9 +4,9 @@
 #include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-char *_strdup(char *s1, char *s2);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-char *_strdup(char *str);
 char *argstostr(int ac, char **av);
 #endif
